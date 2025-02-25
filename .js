ce99c17e87242994e150bf7c4fc817ff56308cb8
Nadiaaa-hub <nadya2000006@gmail.com>
2025-02-25 14:56:33 +0200

    Add the starting files

---
+++ .js
@@ -0,0 +1,10 @@
+document
+  .querySelector(".add-to-cart-button")
+  .addEventListener("click", () => {cartQuantity() });
+
+
+function cartQuantity() { 
+    document.querySelector(".cart-quantity").innerHTML=;
+}
+
+if 
