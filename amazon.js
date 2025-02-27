182f1a62debf04e84b5175424393b64ee52b9f4b
Nadiaaa-hub <nadya2000006@gmail.com>
2025-02-27 16:02:04 +0200

    make the cartQuantity interactive

--- amazon.js
+++ amazon.js
@@ -77,11 +77,13 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
         quantity: 1,
       });
     }
-    console.log(cart);
+
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
   });
 });
-
-// function updateCartQuantity() {
-//   cartQuantity++;
-//   document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
-// }
